8ec35ff2caf1999e3713d6f01cd87fc2c95b799d
Robbie Ferguson <robbiejferguson92@gmail.com>
2021-12-03 20:56:35 +0000

    base.html and dummy index page created. Site is rendering.

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template
 
+# from controllers.transactions_controller import transactions_blueprint
+# from controllers.merchants_controller import merchants_blueprint
+# from controllers.tags_controller import tags_blueprint
+
 app = Flask(__name__)
+
+# app.register_blueprint(transactions_blueprint)
+# app.register_blueprint(merchants_blueprint)
+# app.register_blueprint(tags_blueprint)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
