ed577a93663d84ec07fdd9a104d44e5086f99ca1
Robbie Ferguson <robbiejferguson92@gmail.com>
2021-12-04 14:31:54 +0000

    Date form input now displays on list as DD/MM/YYYY

--- controllers/transactions_controller.py
+++ controllers/transactions_controller.py
@@ -5,7 +5,7 @@ from models.transaction import Transaction
 import repositories.transaction_repository as transaction_repository
 import repositories.merchant_repository as merchant_repository
 import repositories.tag_repository as tag_repository
-from datetime import datetime
+import datetime
 
 transactions_blueprint = Blueprint("transactions", __name__)
 
@@ -23,6 +23,7 @@ def add_transaction():
 @transactions_blueprint.route("/transactions/add", methods=['POST'])
 def new_transaction():
     date = str(request.form['date'])
+    date = datetime.datetime.strptime(date, '%Y-%m-%d').strftime('%d/%m/%Y')
     time = str(request.form['time'])
     amount_spent = float(request.form['amount_spent'])
     merchant = merchant_repository.select_by_name(request.form['merchant'])
