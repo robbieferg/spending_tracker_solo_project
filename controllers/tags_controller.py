d03910174e6e15154940d3925d12c49a9405b52b
Robbie Ferguson <robbiejferguson92@gmail.com>
2021-12-04 08:22:36 +0000

    User can now delete tag from list.

--- controllers/tags_controller.py
+++ controllers/tags_controller.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template, request
 from flask import Blueprint
+from werkzeug.utils import redirect
 from models.tag import Tag
 import repositories.tag_repository as tag_repository
 
 tags_blueprint = Blueprint("tags", __name__)
 
-@tags_blueprint.route("/merchants")
-def merchants():
-    pass
+@tags_blueprint.route("/tags")
+def tags():
+    tags = tag_repository.select_all()
+    return render_template("tags/index.html", tags = tags)
+
+@tags_blueprint.route("/tags/<id>/delete")
+def delete_tag(id):
+    tag_repository.delete(id)
+    return redirect("/tags")
--- controllers/transactions_controller.py
+++ controllers/transactions_controller.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template, request
 from flask import Blueprint
 from models.transaction import Transaction
 import repositories.transaction_repository as transaction_repository
+from datetime import datetime
 
 transactions_blueprint = Blueprint("transactions", __name__)
 
 @transactions_blueprint.route("/transactions")
 def transactions():
-    pass
+    all_transactions = transaction_repository.select_all()
+    for transaction in all_transactions:
+         datetime.strptime(transaction.date)
