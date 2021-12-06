f20c4a11b662203d8f4a349482d2239113b896e9
Robbie Ferguson <robbiejferguson92@gmail.com>
2021-12-06 11:00:07 +0000

    Added select all and update methods to budget repository

--- repositories/budget_repository.py
+++ repositories/budget_repository.py
@@ -1,5 +1,5 @@
 from db.run_sql import run_sql
-from models.tag import Tag
+from models.budget import Budget
 
 def save(budget):
     sql = "INSERT INTO budgets (budget_type, budget_amount) VALUES (%s, %s) RETURNING id"
@@ -7,3 +7,17 @@ def save(budget):
     results = run_sql(sql, values)
     budget.id = results[0]['id']
     return budget
+
+def select_all():
+    budgets = []
+    sql = "SELECT * FROM budgets"
+    results = run_sql(sql)
+    for row in results:
+        budget = Budget(row['budget_type'], row['budget_amount'], row['id'])
+        budgets.append(tag)
+    return budgets
+
+def update(budget, new_budget_type, new_budget_amount):
+    sql = "UPDATE budgets SET (budget_type, budget_amount) = (%s, %s) WHERE id = %s"
+    values = [new_budget_type, new_budget_amount, budget.id]
+    run_sql(sql, values)
